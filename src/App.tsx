0e6f97c52c6a4e3c0c20f2f1c2a5425679940e29
Connor Mooneyhan <connmoo@gmail.com>
2021-11-13 09:57:08 -0500

    refactored footer as Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./css/App.css";
-import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 import Header from "./components/Header/Header";
 import AboutMe from "./components/AboutMe/AboutMe";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
@@ -18,33 +16,7 @@ function App() {
           <a href="./resources/resume-mooneyhan.pdf">resume</a> as a pdf here
         </p>
       </main>
-      <footer className="Footer">
-        <ul className="Footer__contact-info">
-          <li>
-            <a
-              href="https://www.linkedin.com/in/ConnorMooneyhan"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FontAwesomeIcon icon={faLinkedin} />
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://github.com/ConnorMooneyhan"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FontAwesomeIcon icon={faGithub} />
-            </a>
-          </li>
-          <li>
-            <a href="mailto:connmoo@gmail.com">
-              <FontAwesomeIcon icon={faEnvelope} />
-            </a>
-          </li>
-        </ul>
-      </footer>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,37 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+
+const Footer = () => {
+  return (
+    <footer className="Footer">
+      <ul className="Footer__contact-info">
+        <li>
+          <a
+            href="https://www.linkedin.com/in/ConnorMooneyhan"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/ConnorMooneyhan"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+        </li>
+        <li>
+          <a href="mailto:connmoo@gmail.com">
+            <FontAwesomeIcon icon={faEnvelope} />
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
+}
+
+export default Footer;
