228270ae5faa31ede7d01d87600d8a9fea15a061
Connor Mooneyhan <connmoo@gmail.com>
2021-11-13 09:42:04 -0500

    refactored about me section to AboutMe component

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,14 @@ import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 import Header from "./components/Header/Header";
+import AboutMe from "./components/AboutMe/AboutMe";
 
 function App() {
   return (
     <div className="App" style={{ height: window.innerHeight }}>
       <Header />
       <main>
-        <h2>About Me</h2>
-        <p>
-          My passion lies in solving meaningful problems and communicating those solutions to others. This has manifested itself in various areas of interest over the years such as research mathematics, teaching, and now programming, but the core interest has stayed steady and my proficiency has grown through that process.
-        </p>
-        <p>
-          I have a B.S. in Mathematics from Florida State University, and at present I am using the exceptional analytical skills I learned there to develop proficiency in programming, which at present entails mostly React, Node.js, and Rust. I am currently devoting many hours a day, after working my job as a departmental secretary, to honing these skills and shaping myself into a formidable candidate in the field of front-end web development.
-        </p>
-        <p>
-          This new pursuit of mine has been incredibly rewarding and interesting thus far, and I am thrilled to continue absorbing new concepts and cultivating my skills as a web developer as I find my way to my first job in the field.
-        </p>
+        <AboutMe />
         <h2>Resume</h2>
         <p>
           You can download my{" "}
