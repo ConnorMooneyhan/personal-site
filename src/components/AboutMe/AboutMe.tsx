9b05e914d338351840f75a4c59097e105c3b1fa3
Connor Mooneyhan <connmoo@gmail.com>
2021-11-13 09:42:56 -0500

    made AboutMe.tsx a 'section'

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -1,6 +1,6 @@
 const AboutMe = () => {
   return (
-    <>
+    <section>
       <h2>About Me</h2>
       <p>
         My passion lies in solving meaningful problems and communicating those solutions to others. This has manifested itself in various areas of interest over the years such as research mathematics, teaching, and now programming, but the core interest has stayed steady and my proficiency has grown through that process.
@@ -11,7 +11,7 @@ const AboutMe = () => {
       <p>
         This new pursuit of mine has been incredibly rewarding and interesting thus far, and I am thrilled to continue absorbing new concepts and cultivating my skills as a web developer as I find my way to my first job in the field.
       </p>
-    </>
+    </section>
   )
 }
 
