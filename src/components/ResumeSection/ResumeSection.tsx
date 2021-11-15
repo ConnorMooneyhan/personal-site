a3b3d68e4c25232b07e43a2e6dc93dd2dc2fc9fd
Connor Mooneyhan <connmoo@gmail.com>
2021-11-14 21:18:44 -0500

    added tip to use landscape mode if resume won't load

--- src/components/ResumeSection/ResumeSection.tsx
+++ src/components/ResumeSection/ResumeSection.tsx
@@ -8,7 +8,10 @@ const ResumeSection = () => {
     <section className="Resume-Section" id="resume">
       <h2>Resume</h2>
       {resumeFits && <Resume />}
-      {!resumeFits && <h3 style={{color: 'darkred'}}>Unable to fit resume on screen.</h3> }
+      {!resumeFits && [
+        <h3 style={{color: 'darkred'}}>Unable to fit resume on screen.</h3>,
+        <h4 style={{color: 'darkred'}}>(If possible, try rotating your device to landscape mode and reloading the page.)</h4>
+      ]}
       <p>
         You can also <a href="../../resources/resume-mooneyhan.pdf" download>download my resume</a> as a pdf.
       </p>
