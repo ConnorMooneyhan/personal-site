eaa4f2d87465305a0ee9dda7db3074ac61c06198
Connor Mooneyhan <connmoo@gmail.com>
2022-01-16 15:58:37 -0500

    Actually really change resume

--- src/components/ResumeSection/ResumeSection.tsx
+++ src/components/ResumeSection/ResumeSection.tsx
@@ -14,7 +14,7 @@ const ResumeSection = () => {
         <h4 style={{color: 'darkred'}}>(If possible, try rotating your device to landscape mode and reloading the page.)</h4>
       ]}
       <p>
-        You can also <a href="https://github.com/ConnorMooneyhan/ConnorMooneyhan.github.io/raw/main/src/resources/resume-mooneyhan.pdf" download>download my resume</a> as a pdf.
+        You can also <a href="../../resources/resume-mooneyhan.pdf" download>download my resume</a> as a pdf.
       </p>
     </section>
   );
