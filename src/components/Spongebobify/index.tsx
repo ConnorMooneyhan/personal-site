7d4f94c34565dc7ef48e969a6dc7067100cfe742
Connor Mooneyhan <connmoo@gmail.com>
2022-06-28 12:46:26 -0400

    Starting the Spongebobify project

--- src/components/Spongebobify/index.tsx
+++ src/components/Spongebobify/index.tsx
@@ -1,5 +1,18 @@
+import { useState } from "react";
+
 const Spongebobify = () => {
-  return <div></div>;
+  const [text, setText] = useState("");
+
+  return (
+    <div>
+      <input
+        placeholder="text go here"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        className="Spongebobify"
+      />
+    </div>
+  );
 };
 
 export default Spongebobify;
